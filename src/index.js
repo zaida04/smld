e96d990683de364811e06770b5632abeba3f9e34
zaid450 <me@zaid-arshad.tech>
2020-06-18 22:06:39 -0400

    small changes

--- src/index.js
+++ src/index.js
@@ -1,24 +1,35 @@
 const app = require('express')();
-const {static, json} = require('express');
+const {
+	static
+} = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const {domain,port,mongo_uri} = require('./config');
+const {
+	domain,
+	port,
+	mongo_uri
+} = require('./config');
 app.locals.domain = domain;
-mongoose.connect(mongo_uri, {useUnifiedTopology: true,useNewUrlParser: true}, () => console.log('connected to mongo db')); //connect to the DB
+mongoose.connect(mongo_uri, {
+	useUnifiedTopology: true,
+	useNewUrlParser: true
+}, () => console.log('connected to mongo db')); //connect to the DB
 
 let link = require('../models/link');
 app.use(static('public'));
-app.use(json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 app.set('views', __dirname + '/../views');
 app.set('view engine', 'pug');
 //eslint-disable-next-line no-useless-escape
-let url_regex = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+let url_regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
 
 app.get('/', async (_, res) => res.sendFile('index.html'));
 app.post('/b/create', async (req, res) => {
-	if(!req.body.target || !req.body.target.match(url_regex)) return res.send('Not a well formed url');
+	if (!req.body.target || !req.body.target.match(url_regex)) return res.send('Not a well formed url');
 	if (!req.body.target.startsWith('http') || !req.body.target.startsWith('https')) req.body.target = 'http://' + req.body.target;
 	let short = Math.random().toString(36).substring(9);
 	let temp = new link({
@@ -31,9 +42,19 @@ app.post('/b/create', async (req, res) => {
 	});
 });
 app.post('/a/create', async (req, res) => {
-	console.log(req.body);
-	if(!req.body.target || !req.body.target.match(url_regex)) return res.send('Not a well formed url');
+	if (!req.body.target || !req.body.target.match(url_regex)) return res.send('Not a well formed url');
 	if (!req.body.target.startsWith('http') || !req.body.target.startsWith('https')) req.body.target = 'http://' + req.body.target;
+	let check = await link.findOne({
+		'target': req.body.target
+	});
+	if (check) {
+		return res.json({
+			'success': true,
+			'data': {
+				'link': domain + '/' + temp_link.short
+			}
+		});
+	}
 	let short = Math.random().toString(36).substring(9);
 	let temp = new link({
 		'target': req.body.target,
@@ -62,7 +83,7 @@ app.get('/:id/data', async (req, res) => {
 	res.json(temp);
 });
 
-app.delete('/:id', async(req, res) => {
+app.delete('/:id', async (req, res) => {
 	let temp = await link.findOne({
 		'short': req.params.id
 	});
